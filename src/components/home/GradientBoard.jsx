219d213acb33993834dec363288ebc72e4cdbd2f
Aleksandar <aleksandar97kg@gmail.com>
2023-04-26 13:47:42 +0200

    add user login holder

--- src/components/home/GradientBoard.jsx
+++ src/components/home/GradientBoard.jsx
@@ -5,8 +5,13 @@ const GradientBoard = () => {
   return (
     <div className="gradient-board-holder" style={{backgroundImage: `url(${(images.bckgHeaderDesktop)})`}}>
       <div className="holder-content">
-        <h2>Product Feedback</h2>
-        <p>Feedback board</p>
+        <div>
+          <h2>Product Feedback</h2>
+          <p>Feedback board</p>
+        </div>
+        <div className="user-holder">
+          <button type="button" className="btn btn-white">Log in</button>
+        </div>
       </div>
     </div>
   )
